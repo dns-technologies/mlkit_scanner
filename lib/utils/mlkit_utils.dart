5b631247ed0f6771c5488f8be9f000ab81e24105
ilyushin.yai <Ilyushin.YaI>
2023-04-20 16:09:36 +1000

    Review fixes

--- lib/utils/mlkit_utils.dart
+++ lib/utils/mlkit_utils.dart
@@ -3,11 +3,11 @@ import 'package:mlkit_scanner/platform/ml_kit_channel.dart';
 
 /// Contains useful methods that can be accessed regardless of the state of the camera.
 class MLKitUtils {
-  final MlKitChannel _MLKitChannel;
+  final MlKitChannel _mLKitChannel;
 
-  MLKitUtils() : _MLKitChannel = MlKitChannel();
+  MLKitUtils() : _mLKitChannel = MlKitChannel();
 
   /// Gets all available iOS cameras.
   Future<List<IosCamera>> getIosAvailableCameras() =>
-      _MLKitChannel.getIosAvailableCameras();
+      _mLKitChannel.getIosAvailableCameras();
 }
